ee827406d017d71c85358c8503894124065b4903
sudo-prakhar <pkrappdevelopment@gmail.com>
2022-04-24 14:26:16 -0400

    lalala

--- main.py
+++ main.py
@@ -1,23 +1,72 @@
 import numpy as np
-from planner import QuinticPolynomial
+from planner import quintic_polynomials_planner
 
-def make_trajectory():
 
-    sx = 10.0  # start x position [m]
-    sy = 10.0  # start y position [m]
+from autolab_core import RigidTransform
+from frankapy import FrankaArm, SensorDataMessageType
+from frankapy import FrankaConstants as FC
 
-    syaw = np.deg2rad(10.0)  # start yaw angle [rad]
-    sv = 1.0  # start speed [m/s]
-    sa = 0.1  # start accel [m/ss]
-    gx = 30.0  # goal x position [m]
-    gy = -10.0  # goal y position [m]
-    gyaw = np.deg2rad(20.0)  # goal yaw angle [rad]
-    gv = 1.0  # goal speed [m/s]
-    ga = 0.1  # goal accel [m/ss]
-    max_accel = 1.0  # max accel [m/ss]
-    max_jerk = 0.5  # max jerk [m/sss]
-    dt = 0.1  # time tick [s]
-    
+trajectory_ = []
+
+franka_arm = FrankaArm()
+
+sy = franka_arm.translation[1]
+
+########################################### ACCELERATION ###########################################
+sx = 10  # start x position [m]
+sz = 10  # start y position [m]
+
+# sx = franka_arm.translation[0]  # start x position [m]
+# sz = franka_arm.translation[2]  # start y position [m]
+
+syaw = np.deg2rad(0.0)  # start yaw angle [rad]
+sv = 1.0  # start speed [m/s]
+sa = 0.1  # start accel [m/ss]
+
+#! TRAJEC VARS
+gx = 30.0  # goal x position [m]
+gz = 10.0  # goal y position [m]
+gyaw = np.deg2rad(0.0)  # goal yaw angle [rad]
+gv = 1.0  # goal speed [m/s]
+ga = 0.1  # goal accel [m/ss]
+
+
+max_accel = 1.0  # max accel [m/ss]
+max_jerk = 0.5  # max jerk [m/sss]
+dt = 0.1  # time tick [s]
+
+
+time, x, y, yaw, v, a, j = quintic_polynomials_planner(sx, sz, syaw, sv, sa, gx, gz, gyaw, gv, ga, max_accel, max_jerk, dt)
+
+for x_val, y_val, t in zip(x, y, time):
+    trajectory_point = RigidTransform(translation=np.array([sx, sz, sz]), rotation=franka_arm.rotation, from_frame=franka_arm.from_frame, to_frame=franka_arm.from_frame)
+    trajectory_.append(trajectory_point)
+
+########################################### DE-ACCELERATION ###########################################
+sx = gx  # start x position [m]
+sz = gz  # start y position [m]
+syaw = gyaw  # start yaw angle [rad]
+
+sv = 1.0  # start speed [m/s]
+sa = 0.1  # start accel [m/ss]
+gx = 30.0  # goal x position [m]
+gz = 10.0  # goal y position [m]
+
+gyaw = np.deg2rad(0.0)  # goal yaw angle [rad]
+gv = 1.0  # goal speed [m/s]
+ga = 0.1  # goal accel [m/ss]
+max_accel = 1.0  # max accel [m/ss]
+max_jerk = 0.5  # max jerk [m/sss]
+dt = 0.1  # time tick [s]
+
+
+time, x, y, yaw, v, a, j = quintic_polynomials_planner(sx, sz, syaw, sv, sa, gx, gz, gyaw, gv, ga, max_accel, max_jerk, dt)
+
+for x_val, y_val, t in zip(x, y, time):
+    trajectory_point = RigidTransform(translation=np.array([sx, sz, sz]), rotation=franka_arm.rotation, from_frame=franka_arm.from_frame, to_frame=franka_arm.from_frame)
+    trajectory_.append(trajectory_point)
+
+
 
 if __name__ == '__main__':
     pass
